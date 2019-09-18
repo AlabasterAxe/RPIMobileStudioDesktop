21d94fb35e77e496c9367451d8d35acf10d4d864
Matthew Keller <matthew.ed.keller@gmail.com>
2019-09-17 21:11:30 -0400

    fix the oscilloscope build command

--- demos/oscilloscope/build.sh
+++ demos/oscilloscope/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gcc -Wall -ggdb -I../../rpims_v2 -o osc osc.c ../../rpims_v2/.libs/librpims.a -lusb -lGL -lGLU -lglut
+gcc -Wall -ggdb -I../../rpims_v2 -o osc osc.c ../../rpims_v2/librpims.a -lpthread -lusb -lusb-1.0 -lGL -lGLU -lglut
 
